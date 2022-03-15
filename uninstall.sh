a6eb6f3a2afc3f021af997ba1e540234bd66c45c
Joshua Samenfink <joshua@razuuu.de>
2022-03-15 11:15:23 +0000

    TeamSpeak-Client-Installer: Exit script when not using 3 or 5

--- uninstall.sh
+++ uninstall.sh
@@ -9,7 +9,7 @@ fi
 case $1 in
    3) rm -rf /opt/teamspeak3-client; rm -f /usr/share/applications/teamspeak3-client.desktop ;;
    5) rm -rf /opt/teamspeak-client; rm -f /usr/share/applications/teamspeak-client.desktop ;;
-   *) echo "Only 3 or 5 works!"
+   *) echo "Only 3 or 5 works!"; exit 0
 esac
 
 echo "TeamSpeak $1 Client successfully uninstalled!"
