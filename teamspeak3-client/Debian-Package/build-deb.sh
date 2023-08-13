4ed06597c8591bd18d0f72899bb9d96449550cd2
Joshua Samenfink <joshua@razuuu.de>
2023-08-13 04:00:09 +0200

    ts3: optimize build script aswell

--- teamspeak3-client/Debian-Package/build-deb.sh
+++ teamspeak3-client/Debian-Package/build-deb.sh
@@ -3,55 +3,55 @@
 
 ts3version=$1
 
-if [ -z ${ts3version} ]; then
-  echo "bash `basename $0` ts3version"
+if [ -z "$ts3version" ]; then
+  echo "Usage: $(basename "$0") ts3version"
   exit 1
 fi
 
 # Download logo
-curl --progress-bar -O https://raw.githubusercontent.com/Razuuu/TeamSpeak-Client-Installer/master/logo.png
+curl --progress-bar -O "https://raw.githubusercontent.com/Razuuu/TeamSpeak-Client-Installer/master/logo.png"
 
-# Build begins
+# Loop through architectures
 for i in amd64 i386; do
+  if [ "$i" == "i386" ]; then
+    tarch="x86"
+  else
+    tarch="amd64"
+  fi
 
-if [ ${i} == i386 ]; then
-tarch="x86"
-else
-tarch="amd64"
-fi
+  echo "Building Debian package for $i!"
 
-echo "Build Debian package for ${i}!"
+  # Create directory structure
+  build_dir="${ts3version}/${i}"
+  mkdir -p "${build_dir}/opt/teamspeak3-client"
+  cp -r DEBIAN "${build_dir}"
+  cp -r usr "${build_dir}"
 
-mkdir -p ${ts3version}/${i}
-mkdir ${ts3version}/${i}/opt
-cp -r DEBIAN ${ts3version}/${i}
-cp -r usr ${ts3version}/${i}
+  # Replace placeholders in control files
+  sed -i "s|ts3ver|${ts3version}|g" "${build_dir}/DEBIAN/control"
+  sed -i "s|pcarch|${i}|g" "${build_dir}/DEBIAN/control"
+  sed -i "s|ts3ver|${ts3version}|g" "${build_dir}/usr/share/applications/teamspeak3-client.desktop"
 
-sed -i 's|ts3ver|'"$ts3version"'|g' ${ts3version}/${i}/DEBIAN/control
-sed -i 's|pcarch|'"$i"'|g' ${ts3version}/${i}/DEBIAN/control
-sed -i 's|ts3ver|'"$ts3version"'|g' ${ts3version}/${i}/usr/share/applications/teamspeak3-client.desktop
+  # Download and bypass license
+  installation_script="TeamSpeak3-Client-linux_${tarch}-${ts3version}.run"
+  curl --progress-bar -O "https://files.teamspeak-services.com/releases/client/${ts3version}/${installation_script}"
+  sed -i '/^MS_PrintLicense()/,/^}/{s/^/#/}' "${installation_script}"
+  sed -i '/^ *if test $totalsize -ne `expr $fsize - $offset`; then$/,/^ *fi$/s/^/#/' "${installation_script}"
 
-installation_script="TeamSpeak3-Client-linux_${tarch}-${ts3version}.run"
-curl --progress-bar -O https://files.teamspeak-services.com/releases/client/${ts3version}/${installation_script}
+  # Install and copy files
+  bash "${installation_script}" --target "${build_dir}/opt/teamspeak3-client"
+  cp logo.png "${build_dir}/opt/teamspeak3-client"
 
-# Bypass license
-sed -i '/^MS_PrintLicense()/,/^}/{s/^/#/}' "${installation_script}"
-sed -i '/^ *if test $totalsize -ne `expr $fsize - $offset`; then$/,/^ *fi$/s/^/#/' "${installation_script}"
-
-bash ${installation_script} --target ${ts3version}/${i}/opt/teamspeak3-client
-cp logo.png ${ts3version}/${i}/opt/teamspeak3-client
-
-dpkg-deb --build ${ts3version}/${i} teamspeak3-client_${ts3version}_${i}.deb
-echo "done"
-
+  # Create Debian package
+  dpkg-deb --build "${build_dir}" "teamspeak3-client_${ts3version}_${i}.deb"
+  echo "Done building for $i!"
 done
 
-# clean
-rm -rf ${ts3version}
+# Clean up
+rm -rf "${ts3version}"
 rm -f *.run
 rm logo.png
 
 clear
 echo "Done building TeamSpeak 3 Client for amd64 and i386"
-
 exit 0
